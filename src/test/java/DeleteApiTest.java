7fd21d50776f4bf90a50cf2c9c07177cfeb28278
Shrikarsan Logeswaran <shrikarsan@gmail.com>
2023-11-03 17:53:09 +0530

    Print after each test

--- src/test/java/DeleteApiTest.java
+++ src/test/java/DeleteApiTest.java
@@ -15,6 +15,7 @@ public class DeleteApiTest {
 
     private static final String baseURI = APIHelper.baseURI;
     private static final String booksPath = APIHelper.booksPath;
+    private Response response;
 
     @BeforeClass
     public static void initialSetUp() {
@@ -29,62 +30,62 @@ public class DeleteApiTest {
 
     @After
     public void endSetUp() {
-
+        response.prettyPeek();
         APIHelper.deleteAllBooks();
     }
 
     @Test //DEL001
     public void whenDeleteBookByIdWithoutAuth_thenUnauthorized() {
-        Response response = RestAssured
+        response = RestAssured
                 .delete(booksPath + APIHelper.validBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusCode());
     }
 
     @Test //DEL002
     public void whenAdminDeleteBookById_thenOK() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getAdminBasicAuth())
                 .delete(booksPath + APIHelper.validBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
     @Test //DEL003
     public void whenUserDeleteBookById_thenForbidden() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getUserBasicAuth())
                 .delete(booksPath + APIHelper.validBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatusCode());
     }
     @Test //DEL004
     public void whenDeleteBookByInvalidIdWithoutAuth_thenUnauthorized() {
-        Response response = RestAssured
+        response = RestAssured
                 .delete(booksPath + APIHelper.invalidBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusCode());
     }
 
     @Test //DEL005
     public void whenAdminDeleteBookByInvalidId_thenNotFound() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getAdminBasicAuth())
                 .delete(booksPath + APIHelper.invalidBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusCode());
     }
 
     @Test //DEL006
     public void whenUserDeleteBookByInvalidId_thenForbidden() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getUserBasicAuth())
                 .delete(booksPath + APIHelper.invalidBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatusCode());
     }
 
--- src/test/java/GetApiTest.java
+++ src/test/java/GetApiTest.java
@@ -2,6 +2,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import junit.framework.TestCase;
 import org.apache.http.HttpStatus;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,6 +14,7 @@ public class GetApiTest {
 
     private static final String baseURI = APIHelper.baseURI;
     private static final String booksPath = APIHelper.booksPath;
+    private Response response;
 
     @BeforeClass
     public static void initialSetUp() {
@@ -24,87 +26,93 @@ public class GetApiTest {
     public static void endSetUp() {
         APIHelper.deleteAllBooks();
     }
+
+    @After
+    public void printResponse() {
+        response.prettyPeek();
+    }
+
     @Test //GET001
     public void whenGetAllBooksWithoutAuth_thenUnauthorized2() {
-        Response response = RestAssured
+        response = RestAssured
                 .get(booksPath);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusCode());
     }
 
     @Test //GET002
     public void whenAdminGetAllBooks_thenOK() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getAdminBasicAuth())
                 .get(booksPath);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
     @Test //GET003
     public void whenUserGetAllBooks_thenOK() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getUserBasicAuth())
                 .get(booksPath);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
     @Test //GET004
     public void whenGetBookByIdWithoutAuth_thenUnauthorized() {
-        Response response = RestAssured
+        response = RestAssured
                 .get(booksPath + APIHelper.validBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusCode());
     }
 
     @Test //GET005
     public void whenAdminGetBookById_thenOK() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getAdminBasicAuth())
                 .get(booksPath + APIHelper.validBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
     @Test //GET006
     public void whenUserGetBookById_thenOK() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getUserBasicAuth())
                 .get(booksPath + APIHelper.validBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
     }
 
     @Test //GET007
     public void whenGetBookByInvalidIdWithoutAuth_thenUnauthorized() {
-        Response response = RestAssured
+        response = RestAssured
                 .get(booksPath + APIHelper.invalidBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusCode());
     }
 
     @Test //GET008
     public void whenAdminGetBookByInvalidId_thenNotFound() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getAdminBasicAuth())
                 .get(booksPath + APIHelper.invalidBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusCode());
     }
 
     @Test //GET009
     public void whenUserGetBookByInvalidId_thenNotFound() {
-        Response response = RestAssured
+        response = RestAssured
                 .given()
                 .spec(APIHelper.getUserBasicAuth())
                 .get(booksPath + APIHelper.invalidBookId);
-        response.prettyPeek();
+
         TestCase.assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusCode());
     }
 
